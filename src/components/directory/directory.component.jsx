298180f3d32f5e2df2a30a5875fbe6ae91a6f489
harika kota <https://github.com/harika-09/facebook.git>
2020-09-14 13:42:22 -0500

    update menu-items to navigate withRouter Higher Order Components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -7,7 +7,6 @@ import './directory.styles.scss';
 class Directory extends React.Component {
     constructor(){
         super();
-
         this.state = {
             sections :[
                 {
@@ -50,8 +49,8 @@ class Directory extends React.Component {
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title,imageUrl,id,size}) => (
-                        <MenuItem key ={id} title={title}  imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key ={id} {...otherSectionProps} />
                     ))
                 }
             </div>
