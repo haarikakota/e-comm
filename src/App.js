fe5be5c368b1f3bd5f20c51fda4a5fd6997ad3ce
harika kota <https://github.com/harika-09/facebook.git>
2020-09-14 20:58:27 -0500

    signin and signup components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component'
+import SignInSignUpPage from './pages/sign-in-and-sign-up/signin-signup.component';
+import Header from './components/header/header.component';
+
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route  path='/shop' component={ShopPage}/>
+        <Route  path='/signin' component={SignInSignUpPage}/>
+
       </Switch>
     </div> 
   );
